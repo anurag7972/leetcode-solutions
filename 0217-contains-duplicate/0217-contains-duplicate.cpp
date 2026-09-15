18f001854bc5e2d6c13fcb108b42fd407a52b01a
Anurag Singh <anuragsinghsingh589@gmail.com>
2026-09-15 12:28:37 +0530

    Time: 42 ms (77.48%), Space: 82.9 MB (80.36%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -4,8 +4,8 @@ public:
 
         int n=nums.size();
         sort(nums.begin(), nums.end());
-        for(int i=1; i<n; i++){
-            if(nums[i]==nums[i-1]){
+        for(int i=0; i<n-1; i++){
+            if(nums[i]==nums[i+1]){
                 return true;
             }
         }
